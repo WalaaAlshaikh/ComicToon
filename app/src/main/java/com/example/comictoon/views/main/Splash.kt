39b6129e8691e5dbb1689300ea9bf79b1162a942
Walaa Alshaikh <walaalshaikh94@gmail.com>
2021-12-08 11:58:48 +0300

    adding the splash

--- app/src/main/java/com/example/comictoon/views/main/Splash.kt
+++ app/src/main/java/com/example/comictoon/views/main/Splash.kt
@@ -1,13 +1,30 @@
 package com.example.comictoon.views.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.example.comictoon.R
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        intent= Intent(this,MainActivity::class.java)
+        object: CountDownTimer(1000,1000){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+               startActivity(intent)
+
+            }
+
+        }.start()
     }
-    /////////jskw
+
+
 }
